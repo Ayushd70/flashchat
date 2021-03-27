0e20f2add3bc58ea9f58f9ee5b19c23743462d75
Ayush Dubey <ayushdubey70@gmail.com>
2021-03-27 21:06:46 +0530

    flashchat: use static constant for routes
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,12 @@ class FlashChat extends StatelessWidget {
           bodyText2: TextStyle(color: Colors.black54),
         ),
       ),
-      initialRoute: 'welcome_screen',
+      initialRoute: WelcomeScreen.id;
       routes: {
-        'welcome_screen': (context) => WelcomeScreen(),
-        'login_screen': (context) => LoginScreen(),
-        'registration_screen': (context) => RegistrationScreen(),
-        'chat_screen': (context) => ChatScreen(),
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
       },
     );
   }
