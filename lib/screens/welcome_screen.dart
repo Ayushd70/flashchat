42802bbb0dd042a1a0a73e5ecef38feae7d17e56
Ayush Dubey <ayushdubey70@gmail.com>
2021-03-30 19:48:58 +0530

    flashchat: change begining color of ColorTween to blueGrey
    *blueGrey looks much better
    
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -24,8 +24,8 @@ class _WelcomeScreenState extends State<WelcomeScreen>
       vsync: this,
       duration: Duration(seconds: 3),
     );
-    animation =
-        ColorTween(begin: Colors.red, end: Colors.white).animate(controller);
+    animation = ColorTween(begin: Colors.blueGrey, end: Colors.white)
+        .animate(controller);
     controller.forward();
 
     controller.addListener(() {
