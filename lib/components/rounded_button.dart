75b92842ec85c47bef5dd1df7e5652bc89524cd1
Ayush Dubey <ayushdubey70@gmail.com>
2021-03-31 20:33:00 +0530

    flashchat: rounded_button: change the text color to white
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/components/rounded_button.dart
+++ lib/components/rounded_button.dart
@@ -21,6 +21,9 @@ class RoundedButton extends StatelessWidget {
           height: 42.0,
           child: Text(
             title,
+            style: TextStyle(
+              color: Colors.white,
+            ),
           ),
         ),
       ),
