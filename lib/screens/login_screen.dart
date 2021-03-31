62209f990d7d005b77f81d414c7dad7819af6eb1
Ayush Dubey <ayushdubey70@gmail.com>
2021-03-31 20:11:34 +0530

    flashchat: login_screen: refactor padding widget to use Rounded Button
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flashchat/components/rounded_button.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -80,23 +81,12 @@ class _LoginScreenState extends State<LoginScreen> {
             SizedBox(
               height: 24.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    //Implement login functionality.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
-              ),
+            RoundedButton(
+              title: 'Log In',
+              colour: Colors.lightBlueAccent,
+              onPressed: () {
+                //Implement login functionality.
+              },
             ),
           ],
         ),
