037e7191a1a8a645b9db18e84669bd357b3d4bf6
Ayush Dubey <ayushdubey70@gmail.com>
2021-04-02 20:35:07 +0530

    flashchat: registration: use keyboardstyle to show up @ symbol
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -33,6 +33,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 48.0,
             ),
             TextField(
+                keyboardType: TextInputType.emailAddress,
                 textAlign: TextAlign.center,
                 onChanged: (value) {
                   email = value;
