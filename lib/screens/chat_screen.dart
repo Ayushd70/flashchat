cfe8499899261a78c8de4f355362dcbbd966fc5d
Ayush Dubey <ayushdubey70@gmail.com>
2021-04-04 20:49:27 +0530

    flashchat: chat: don't print the user email in console
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -25,7 +25,6 @@ class _ChatScreenState extends State<ChatScreen> {
       final user = await _auth.currentUser;
       if (user != null) {
         loggedInUser = user;
-        print(loggedInUser.email);
       }
     } catch (e) {
       print(e);
