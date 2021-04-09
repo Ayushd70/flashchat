90de3db418e75a7a6da8a496965e09e87aa222a3
Ayush Dubey <ayushdubey70@gmail.com>
2021-04-09 20:59:05 +0530

    flashchat: chat: fix the order of messages
    *show the recent most messages at the bottom
    *show the listview at the bottom
    
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -124,7 +124,7 @@ class MessagesStream extends StatelessWidget {
             ),
           );
         }
-        final messages = snapshot.data.docs;
+        final messages = snapshot.data.docs.reversed;
         List<MessageBubble> messageBubbles = [];
         for (var message in messages) {
           final messageText = message.data()['text'];
@@ -141,6 +141,7 @@ class MessagesStream extends StatelessWidget {
         }
         return Expanded(
           child: ListView(
+            reverse: true,
             padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
             children: messageBubbles,
           ),
